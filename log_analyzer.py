68f2b3d98011ad06c67f2798fad3d178f33a3cd7
ibukin <ibukin@voice-link.ru>
2022-03-14 19:13:31 +0300

    Add logging and use template

--- log_analyzer.py
+++ log_analyzer.py
@@ -7,18 +7,21 @@
 import argparse
 import configparser
 import datetime
+import logging
 import os
 import fnmatch
 import gzip
 import re
 from collections import defaultdict
 from statistics import mean, median
+from string import Template
 
 config = {
     "REPORT_SIZE": 1000,
     "REPORT_DIR": "./reports",
     "LOG_DIR": "./log",
-    "ALLOW_PERC_ERRORS": 50
+    "ALLOW_PERC_ERRORS": 50,
+    "LOGGING_FILE": None
 }
 
 
@@ -26,40 +29,60 @@ def load_config(config):
     parser = argparse.ArgumentParser()
     parser.add_argument("--config", default="./config.cfg", type=str, help="Path to config")
     args = parser.parse_args()
-    config = configparser.ConfigParser(config)
-    config.read(args.config)
-    return config
+    if os.path.isfile(args.config):
+        config = configparser.ConfigParser(config, allow_no_value=True)
+        config.read(args.config)
+        logging.info(f"Load config file {args.config}")
+        return config
+    else:
+        raise FileExistsError(f"Cant load config file {args.config}")
 
 
-def check_exist_reports(pat, report_dir):
+cfg = load_config(config)
+logging.basicConfig(format='[%(asctime)s] %(levelname)s %(message)s',
+                    datefmt="%Y.%m.%d %H:%M:%S",
+                    filename=cfg.get("Settings", "LOGGING_FILE"),
+                    encoding='utf-8',
+                    level=logging.DEBUG)
+logger = logging.getLogger()
+
+
+def check_exist_report(pat, report_dir):
     date = datetime.datetime.strptime(pat, '%Y%m%d')
     pat = date.strftime("%Y.%m.%d")
-    for path, dirlist, filelist in os.walk(report_dir):
-        for name in fnmatch.filter(filelist, f"report-{pat}.html"):
-            return os.path.join(path, name)
-    return None
+    file_path = os.path.join(report_dir, f"report-{pat}.html")
+    return date, os.path.isfile(file_path)
 
 
 def find_last_date_log(log_dir, report_dir):
     for path, dirlist, filelist in os.walk(log_dir):
         date_list = re.findall(r'\d+', str(filelist))
         pat = max(date_list)
-        report = check_exist_reports(pat, report_dir)
-        if report:
-            return report
+        date, report_exist = check_exist_report(pat, report_dir)
+        if report_exist:
+            logging.warning(f"last date log ({pat}) report exists")
+            raise FileExistsError
         else:
             for name in fnmatch.filter(filelist, f"nginx-access-ui.log-{pat}.*"):
-                return os.path.join(path, name)
+                logging.info(f"last date log found: {name}")
+                return date, os.path.join(path, name)
 
 
 def log_open(filename):
-    file = gzip.open(filename) if filename.endswith(".gz") else open(filename)
-    return file
+    try:
+        file = gzip.open(filename) if filename.endswith(".gz") else open(filename)
+        return file
+    except UnicodeDecodeError as error:
+        logger.exception(f"Cant read log file: {filename}\nERROR: {error}")
+        return None
+    except Exception as error:
+        logger.exception(f"Unknown error while reading log file: {filename}\nERROR: {error}")
+        raise error.__class__
 
 
 def log_lines(log_file):
-        for item in log_file:
-            yield item
+    for item in log_file:
+        yield item
 
 
 def log_parser(lines):
@@ -69,14 +92,13 @@ def log_parser(lines):
     for line in lines:
         data = re.search(logpat, line.decode())
         if data:
-            print("GOOD",line)
             yield (data.groupdict())
         else:
-            print("BAD",line)
             yield None
 
 
 def collect_report_data(log_parse, allow_perc_error):
+    logger.info(f"Start collect report data")
     res = defaultdict(dict)
     count_all_time = 0
     count_none_line = 0
@@ -93,9 +115,11 @@ def collect_report_data(log_parse, allow_perc_error):
         else:
             count_none_line += 1
 
-    if count_none_line > len(res)*(allow_perc_error * 0.01):
-        print(f"Too many errors while reading the file:")
+    # if count_none_line > len(res) * (allow_perc_error * 0.01):
+    #     logger.warning(f"Too many errors while reading file\nAllow percent errors: {allow_perc_error}%")
+    #     raise ValueError
 
+    logger.info(f"Complete collect data to report\nLog processed:{len(res)}\nLog unread:{count_none_line}")
     return res, count_all_time
 
 
@@ -103,26 +127,40 @@ def create_report(report_data, count_all_time):
     len_data = len(report_data)
     for url, info in report_data.items():
         res = {
+            "url": url,
             "count": info["count"],
-            "count_perc": (info["count"]/len_data * 100).__round__(3),
-            "time_sum": sum(info["list_request_time"]).__round__(3),
-            "time_perc": (sum(info["list_request_time"])/count_all_time * 100).__round__(3),
+            "count_perc": (info["count"] / len_data * 100).__round__(3),
             "time_avg": mean(info["list_request_time"]).__round__(3),
             "time_max": max(info["list_request_time"]),
-            "time_med": median(info["list_request_time"]).__round__(3)
+            "time_med": median(info["list_request_time"]).__round__(3),
+            "time_perc": (sum(info["list_request_time"]) / count_all_time * 100).__round__(3),
+            "time_sum": sum(info["list_request_time"]).__round__(3),
         }
         yield res
 
 
-def main():
-    cfg = load_config(config)
-    filename = find_last_date_log(cfg.get("Settings", "LOG_DIR"), cfg.get("Settings", "REPORT_DIR"))
+def render_report(cfg, date, report):
+    with open("./reports/report.html", "r") as report_template:
+        template = Template(report_template.read())
+        res = template.safe_substitute(table_json=list(report))
+
+        pat = date.strftime("%Y.%m.%d")
+        file_path = os.path.join(cfg.get("Settings", "REPORT_DIR"), f"report-{pat}.html")
+
+        report_file = open(file_path, "w")
+        report_file.write(res)
+        report_file.close()
+
+
+def main(cfg):
+    date, filename = find_last_date_log(cfg.get("Settings", "LOG_DIR"), cfg.get("Settings", "REPORT_DIR"))
     logfiles = log_open(filename)
     loglines = log_lines(logfiles)
     log_parse = log_parser(loglines)
     report_data, count_all_time = collect_report_data(log_parse, cfg.get("Settings", "ALLOW_PERC_ERRORS"))
-    # report = create_report(report_data, count_all_time)
+    report = create_report(report_data, count_all_time)
+    render_report(cfg, date, report)
 
 
 if __name__ == "__main__":
-    main()
+    main(cfg)
