85a919b294be979d540323882ef105a400df7813
ibukin <ibukin@voice-link.ru>
2022-04-06 22:05:16 +0300

    Review tests

--- log_analyzer.py
+++ log_analyzer.py
@@ -29,6 +29,9 @@ default_config = {
 
 logger = logging.getLogger()
 
+logpats = r'(?P<ipaddress>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})([ ](?P<xerb>.+)[ ]) - \[(?P<dateandtime>\d{2}\/[A-z]{3}\/\d{4}:\d{2}:\d{2}:\d{2} (\+|\-)\d{4})\] ((\"(GET|POST) )(?P<url>.+)(HTTP\/1\.\S")) (?P<statuscode>\d{3}) (?P<bytessent>\d+) (["](?P<refferer>(\-)|(.+))["]) (["](?P<useragent>.+)["]) (?P<request_time>[+-]?([0-9]*[.])?[0-9]+)'
+LOGPAT = re.compile(logpats, re.IGNORECASE)
+
 
 def load_config(default_config, args):
     if os.path.isfile(args.config):
@@ -52,14 +55,13 @@ def check_exist_report(pat, report_dir):
         msg = f"last date log ({pat}) report exists"
         logging.error(msg)
         raise FileExistsError(msg)
-    print(date)
+
     return date
 
 
 def find_last_date_log(log_dir):
     filelist = os.listdir(log_dir)
     date_list = re.findall(r'\d+', str(filelist))
-    logger.error(date_list)
     pat = max(date_list)
     for name in fnmatch.filter(filelist, f"nginx-access-ui.log-{pat}.*"):
         logging.info(f"last date log found: {name}")
@@ -81,11 +83,8 @@ def log_lines(log_file):
 
 
 def log_parser(lines):
-    logpats = r'(?P<ipaddress>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})([ ](?P<xerb>.+)[ ]) - \[(?P<dateandtime>\d{2}\/[A-z]{3}\/\d{4}:\d{2}:\d{2}:\d{2} (\+|\-)\d{4})\] ((\"(GET|POST) )(?P<url>.+)(HTTP\/1\.\S")) (?P<statuscode>\d{3}) (?P<bytessent>\d+) (["](?P<refferer>(\-)|(.+))["]) (["](?P<useragent>.+)["]) (?P<request_time>[+-]?([0-9]*[.])?[0-9]+)'
-    logpat = re.compile(logpats, re.IGNORECASE)
-
     for line in lines:
-        data = re.search(logpat, line.decode())
+        data = re.search(LOGPAT, line.decode())
         if data:
             yield (data.groupdict())
         else:
@@ -136,15 +135,19 @@ def create_report(report_data, count_all_time):
     logger.info(f"Complete create report. Log: {len(report_data)}")
 
 
-def render_report(cfg, str_date, report):
+def get_path_report(str_date):
+    date = datetime.datetime.strptime(str_date, '%Y%m%d')
+    pat = date.strftime("%Y.%m.%d")
+    file_path = os.path.join(cfg.get("Settings", "REPORT_DIR"), f"report-{pat}.html")
+    return file_path
+
+
+def render_report(cfg, file_path, report):
     with open("./reports/report.html", "r") as report_template:
         template = Template(report_template.read())
         report_size = int(cfg.get("Settings", "REPORT_SIZE"))
         sorted_report = sorted(list(report), key=itemgetter('time_sum'), reverse=True)
         res = template.safe_substitute(table_json=sorted_report[0:report_size])
-        date = datetime.datetime.strptime(str_date, '%Y%m%d')
-        pat = date.strftime("%Y.%m.%d")
-        file_path = os.path.join(cfg.get("Settings", "REPORT_DIR"), f"report-{pat}.html")
 
         report_file = open(file_path, "w")
         report_file.write(res)
@@ -156,11 +159,12 @@ def main(cfg):
     pat, last_log = find_last_date_log(cfg.get("Settings", "LOG_DIR"))
     logfiles = log_open(last_log)
     check_exist_report(pat, cfg.get("Settings", "REPORT_DIR"))
-    loglines = log_lines(logfiles)
+    loglines = (i for i in logfiles)
     log_parse = log_parser(loglines)
     report_data, count_all_time = collect_report_data(log_parse, cfg.get("Settings", "ALLOW_PERC_ERRORS"))
     report = create_report(report_data, count_all_time)
-    render_report(cfg, pat, report)
+    path_report = get_path_report(pat)
+    render_report(cfg, path_report, report)
 
 
 if __name__ == "__main__":
@@ -176,4 +180,7 @@ if __name__ == "__main__":
         level=logging.DEBUG
     )
     logger = logging.getLogger()
-    main(cfg)
+    try:
+        main(cfg)
+    except Exception as error:
+        logger.error(f"Unknown error from My_log_analyzer. ERROR: {error}")
